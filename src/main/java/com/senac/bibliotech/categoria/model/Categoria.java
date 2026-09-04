35880ddf0d81944a009ff1afe52f33e3ac45a24d
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-09-04 15:20:44 -0300

    feat: adiciona MER bibliotech

--- src/main/java/com/senac/bibliotech/categoria/model/Categoria.java
+++ src/main/java/com/senac/bibliotech/categoria/model/Categoria.java
@@ -1,4 +1,15 @@
 package com.senac.bibliotech.categoria.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private
 }
