6fabee66021b9c29ade918b6614de6776039edd9
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-09-04 19:35:27 -0300

    feat: adiciona saveLivro com livromapper

---
+++ src/main/java/com/senac/bibliotech/livro/mapper/LivroMapper.java
@@ -0,0 +1,18 @@
+package com.senac.bibliotech.livro.mapper;
+
+
+import com.senac.bibliotech.livro.api.request.LivroRequest;
+import com.senac.bibliotech.livro.api.response.LivroResponse;
+import com.senac.bibliotech.livro.domain.Livro;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface LivroMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "autor", ignore = true)
+    @Mapping(target = "categoria", ignore = true)
+    Livro createLivro(LivroRequest livroRequest);
+}
--- src/main/java/com/senac/bibliotech/livro/service/LivroService.java
+++ src/main/java/com/senac/bibliotech/livro/service/LivroService.java
@@ -1,7 +1,16 @@
 package com.senac.bibliotech.livro.service;
 
 
+import com.senac.bibliotech.autor.domain.Autor;
+import com.senac.bibliotech.autor.repository.AutorRepository;
+import com.senac.bibliotech.autor.service.AutorService;
+import com.senac.bibliotech.categoria.api.request.CategoriaRequest;
+import com.senac.bibliotech.categoria.model.Categoria;
+import com.senac.bibliotech.categoria.repository.CategoriaRepository;
+import com.senac.bibliotech.categoria.service.CategoriaService;
+import com.senac.bibliotech.livro.api.request.LivroRequest;
 import com.senac.bibliotech.livro.domain.Livro;
+import com.senac.bibliotech.livro.mapper.LivroMapper;
 import com.senac.bibliotech.livro.repository.LivroRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +20,31 @@ import java.util.List;
 public class LivroService {
 
     private final LivroRepository livroRepository;
+    private final LivroMapper livroMapper;
+    private final AutorService autorService;
+    private final CategoriaService categoriaService;
 
-
-    public LivroService(LivroRepository livroRepository) {
+    public LivroService(LivroRepository livroRepository, LivroMapper livroMapper, AutorService autorService, CategoriaService categoriaService) {
         this.livroRepository = livroRepository;
+        this.livroMapper = livroMapper;
+        this.autorService = autorService;
+        this.categoriaService = categoriaService;
     }
+
 
     public List<Livro> findAll(){
         return livroRepository.findAll();
     }
 
-//    public Livro save(LivroRequest livroRequest){
-//
-//
-//    }
+    public Livro save(LivroRequest livroRequest){
+        Autor autor = autorService.findById(livroRequest.autorId());
+        Categoria categoria = categoriaService.findCategoriaEntityById(livroRequest.categoriaId());
+
+        Livro livro = livroMapper.createLivro(livroRequest);
+        livro.setAutor(autor);
+        livro.setCategoria(categoria);
+
+        return livroRepository.save(livro);
+    }
 
 }
