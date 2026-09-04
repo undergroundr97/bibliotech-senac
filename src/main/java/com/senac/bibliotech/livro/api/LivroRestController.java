60deb18a8b834be13160998e4677de99f7e38de7
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-09-04 19:37:59 -0300

    feat: adiciona endpoint post

--- src/main/java/com/senac/bibliotech/livro/api/LivroRestController.java
+++ src/main/java/com/senac/bibliotech/livro/api/LivroRestController.java
@@ -1,11 +1,15 @@
 package com.senac.bibliotech.livro.api;
 
 
+import com.senac.bibliotech.livro.api.request.LivroRequest;
 import com.senac.bibliotech.livro.domain.Livro;
 import com.senac.bibliotech.livro.service.LivroService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,12 +29,24 @@ public class LivroRestController {
         return ResponseEntity.ok(livroService.findAll());
     }
 
-//    @PostMapping
-//    public ResponseEntity<Livro> postLivr(
-//            @Valid
-//            @RequestBody
-//            LivroRequest livroRequest){
-//
-//    }
+    @PostMapping
+    public ResponseEntity<Livro> postLivr(
+            @Valid
+            @RequestBody
+            LivroRequest livroRequest){
+
+        Livro livro = livroService.save(livroRequest);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(livro)
+                .toUri();
+
+        return ResponseEntity.created(uri).body(livro);
+
+    }
+
+    public
 
 }
