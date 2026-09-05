72de2c4d58c312d74308645ef657e8844df6490f
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-09-04 21:51:05 -0300

    feat: adiciona postEndPoint

--- src/main/java/com/senac/bibliotech/secao/api/SecaoController.java
+++ src/main/java/com/senac/bibliotech/secao/api/SecaoController.java
@@ -8,7 +8,9 @@ import com.senac.bibliotech.secao.service.SecaoService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -37,7 +39,14 @@ public class SecaoController {
             @RequestBody
             SecaoRequest secaoRequest
                                                     ){
+        SecaoResponse secaoResponse = secaoService.postSecao(secaoRequest);
 
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(secaoResponse.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(secaoResponse);
     }
 
 }
