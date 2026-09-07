d4de02b55a4a95c3b1a12b30fe53d919e188027f
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-09-07 19:33:12 -0300

    feat: adiciona resourcenotfound exception global

--- src/main/java/com/senac/bibliotech/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/senac/bibliotech/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.senac.bibliotech.exceptions;
 
 
 import com.senac.bibliotech.exceptions.customexceptions.BibliotecaNotFound;
+import com.senac.bibliotech.exceptions.customexceptions.ResourceNotFound;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +14,14 @@ import java.time.Instant;
 @RestControllerAdvice
 public class ResourceExceptionHandler {
 
-
-    @ExceptionHandler(BibliotecaNotFound.class)
-    public ResponseEntity<StandardError> BibliotecaNotFound(BibliotecaNotFound e, HttpServletRequest request){
-
-        String error = "Biblioteca not found";
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<StandardError> bibliotecaNotFound(BibliotecaNotFound e, HttpServletRequest request){
+        String error = "Resource Not Found.";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
                 request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
 
 }
